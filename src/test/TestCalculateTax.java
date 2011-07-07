22531313887dcc92a270937dcd29f917e02e042d
Rajashree Malvade <rajashreermalvade@gmail.com>
2011-07-07 15:55:58 +0530

    Refactored test class (TestCalculateTax.java) to remove repitition

--- src/test/TestCalculateTax.java
+++ src/test/TestCalculateTax.java
@@ -3,6 +3,8 @@ import main.Item;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by IntelliJ IDEA.
  * User: rajashre
@@ -14,18 +16,29 @@ public class TestCalculateTax {
     @Test
     public void costOfOneBookWithPriceTenIsTen() throws Exception
     {
-        Assert.assertEquals(10.0, new Item().calculateCostAfterTax("book", 1, 10.0));
+        assertEquals(10.0,calculateTotalCostOfItem("book",1,10.0));
     }
 
     @Test
     public void costOfBookWithPriceFifteenIsFifteen() throws Exception
     {
-        Assert.assertEquals(15.0, new Item().calculateCostAfterTax("book",1,15.0));
+        Assert.assertEquals(15.0,calculateTotalCostOfItem("book",1,15.0) );
     }
 
     @Test
     public void costOfTwoBooksWithPriceFifteenIsThirty() throws Exception
     {
-        Assert.assertEquals(30.0, new Item().calculateCostAfterTax("book",2,15.0));
+        Assert.assertEquals(30.0, calculateTotalCostOfItem("book", 2, 15.0));
+    }
+
+    @Test
+    public void costOfBookWithPriceTwelvePonitFourtyNineIsThirty() throws Exception
+    {
+        Assert.assertEquals(12.49, calculateTotalCostOfItem("book",1,12.49));
+    }
+
+    private double calculateTotalCostOfItem(String book, int quantity, double price)
+    {
+        return new Item().calculateCostAfterTax(book, quantity, price);
     }
 }
